142640d56dcaed256139f37f259383bcb80736a3
ac2522 <52031988+ac2522@users.noreply.github.com>
2023-05-01 11:48:57 +0100

    Delete mangaScrape directory

--- mangaScrape/zoro.js
+++
@@ -1,7 +0,0 @@
-try {
-    const serverNotice = document.querySelector(".server-notice");
-    const episodeNumber = parseInt(serverNotice.querySelector("b")?.textContent?.match(/Episode (\d+)/)?.[1]);
-    chrome.runtime.sendMessage({episodeNumber: episodeNumber});
-} catch (error) {
-    chrome.runtime.sendMessage({error: error.message});
-}
